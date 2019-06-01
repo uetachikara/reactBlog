6575a2e34bbaec2a35c4f08d6f9daba9ebc6a1d8
植田智加来 <yakyuzukiueta@gmail.com>
2019-06-01 13:47:32 +0900

    Blogコンポーネントが表示されない

---
+++ app/javascript/packs/blog-bundle.js
@@ -0,0 +1,7 @@
+import ReactOnRails from 'react-on-rails'
+import Blog from '../bundles/Blog/components/Blog'
+
+
+ReactOnRails.register({
+  Blog,
+});
--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,5 +1,4 @@
 import ReactOnRails from 'react-on-rails';
-
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
